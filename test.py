0ed7c8c0fa9c2f3f1509614fbf49771f795eb0bb
azdle <azdle@azdle.net>
2022-10-31 14:23:56 -0500

    add failing output test

--- test.py
+++ test.py
@@ -5,3 +5,13 @@ from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 @cocotb.test()
 async def test_my_design(dut):
     dut._log.info("start")
+
+    clock = Clock(dut.CLK, 1, units="ms")
+    cocotb.start_soon(clock.start())
+
+    dut.RST.value = 1
+    await ClockCycles(dut.CLK, 10)
+    dut.RST = 0
+
+    await ClockCycles(dut.CLK, 1)
+    assert dut.out.value.integer == 0b0000_0000
